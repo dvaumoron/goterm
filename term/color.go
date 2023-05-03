23c2cfce2ae9a481af94d11100d1aa2efdfae25c
dvaumoron <dvaumoron@gmail.com>
2023-05-03 08:28:59 +0200

    extending API for byte slice

--- term/color.go
+++ term/color.go
@@ -193,12 +193,32 @@ func colConcat(mode string, s string, nMode string) string {
 	return buffer.String()
 }
 
+// colConcatByte add beginning and ending color mode modifer (byte slice)
+func colConcatByte(mode string, b []byte, nMode string) []byte {
+	if !colorEnable {
+		return b
+	}
+	buffer := make([]byte, 0, baseLen+len(mode)+len(b)+len(nMode))
+	buffer = append(buffer, CSI...)
+	buffer = append(buffer, mode...)
+	buffer = append(buffer, endCSI)
+	buffer = append(buffer, b...)
+	buffer = append(buffer, CSI...)
+	buffer = append(buffer, nMode...)
+	buffer = append(buffer, endCSI)
+	return buffer
+}
+
 // Stringers for all the base colors , just fill it in with something and print it
 // Foreground
 
 // String implements the Stringer interface for type Green.
 func (c Green) String() string {
 	return colConcat(FgGreen, string(c), FgDefault)
+}
+
+func GreenByte(b []byte) []byte {
+	return colConcatByte(FgGreen, b, FgDefault)
 }
 
 // Greenf returns a Green formatted string.
@@ -211,6 +231,10 @@ func (c Blue) String() string {
 	return colConcat(FgBlue, string(c), FgDefault)
 }
 
+func BlueByte(b []byte) []byte {
+	return colConcatByte(FgBlue, b, FgDefault)
+}
+
 // Bluef returns a Blue formatted string.
 func Bluef(format string, a ...any) string {
 	return Blue(fmt.Sprintf(format, a...)).String()
@@ -219,6 +243,10 @@ func Bluef(format string, a ...any) string {
 // String implements the Stringer interface for type Red.
 func (c Red) String() string {
 	return colConcat(FgRed, string(c), FgDefault)
+}
+
+func RedByte(b []byte) []byte {
+	return colConcatByte(FgRed, b, FgDefault)
 }
 
 // Redf returns a Red formatted string.
@@ -231,6 +259,10 @@ func (c Yellow) String() string {
 	return colConcat(FgYellow, string(c), FgDefault)
 }
 
+func YellowByte(b []byte) []byte {
+	return colConcatByte(FgYellow, b, FgDefault)
+}
+
 // Yellowf returns a Yellow formatted string.
 func Yellowf(format string, a ...any) string {
 	return Yellow(fmt.Sprintf(format, a...)).String()
@@ -239,6 +271,10 @@ func Yellowf(format string, a ...any) string {
 // String implements the Stringer interface for type Magenta.
 func (c Magenta) String() string {
 	return colConcat(FgMagenta, string(c), FgDefault)
+}
+
+func MagentaByte(b []byte) []byte {
+	return colConcatByte(FgMagenta, b, FgDefault)
 }
 
 // Magentaf returns a Magenta formatted string.
@@ -251,6 +287,10 @@ func (c White) String() string {
 	return colConcat(FgWhite, string(c), FgDefault)
 }
 
+func WhiteByte(b []byte) []byte {
+	return colConcatByte(FgWhite, b, FgDefault)
+}
+
 // Whitef returns a White formatted string.
 func Whitef(format string, a ...any) string {
 	return White(fmt.Sprintf(format, a...)).String()
@@ -261,6 +301,10 @@ func (c Black) String() string {
 	return colConcat(FgBlack, string(c), FgDefault)
 }
 
+func BlackByte(b []byte) []byte {
+	return colConcatByte(FgBlack, b, FgDefault)
+}
+
 // Blackf returns a Black formatted string.
 func Blackf(format string, a ...any) string {
 	return Black(fmt.Sprintf(format, a...)).String()
@@ -269,6 +313,10 @@ func Blackf(format string, a ...any) string {
 // String implements the Stringer interface for type Cyan.
 func (c Cyan) String() string {
 	return colConcat(FgCyan, string(c), FgDefault)
+}
+
+func CyanByte(b []byte) []byte {
+	return colConcatByte(FgCyan, b, FgDefault)
 }
 
 // Cyanf returns a Cyan formatted string.
@@ -283,6 +331,10 @@ func (c BGreen) String() string {
 	return colConcat(BgGreen, string(c), BgDefault)
 }
 
+func BGreenByte(b []byte) []byte {
+	return colConcatByte(BgGreen, b, BgDefault)
+}
+
 // BGreenf returns a BGreen formatted string.
 func BGreenf(format string, a ...any) string {
 	return BGreen(fmt.Sprintf(format, a...)).String()
@@ -291,6 +343,10 @@ func BGreenf(format string, a ...any) string {
 // String implements the Stringer interface for type BBlue.
 func (c BBlue) String() string {
 	return colConcat(BgBlue, string(c), BgDefault)
+}
+
+func BBlueByte(b []byte) []byte {
+	return colConcatByte(BgBlue, b, BgDefault)
 }
 
 // BBluef returns a BBlue formatted string.
@@ -303,6 +359,10 @@ func (c BRed) String() string {
 	return colConcat(BgRed, string(c), BgDefault)
 }
 
+func BRedByte(b []byte) []byte {
+	return colConcatByte(BgRed, b, BgDefault)
+}
+
 // BRedf returns a BRed formatted string.
 func BRedf(format string, a ...any) string {
 	return BRed(fmt.Sprintf(format, a...)).String()
@@ -311,6 +371,10 @@ func BRedf(format string, a ...any) string {
 // String implements the Stringer interface for type BYellow.
 func (c BYellow) String() string {
 	return colConcat(BgYellow, string(c), BgDefault)
+}
+
+func BYellowGreenByte(b []byte) []byte {
+	return colConcatByte(BgYellow, b, BgDefault)
 }
 
 // BYellowf returns a BYellow formatted string.
@@ -323,6 +387,10 @@ func (c BMagenta) String() string {
 	return colConcat(BgMagenta, string(c), BgDefault)
 }
 
+func BMagentaByte(b []byte) []byte {
+	return colConcatByte(BgMagenta, b, BgDefault)
+}
+
 // BMagentaf returns a BMagenta formatted string.
 func BMagentaf(format string, a ...any) string {
 	return BMagenta(fmt.Sprintf(format, a...)).String()
@@ -331,6 +399,10 @@ func BMagentaf(format string, a ...any) string {
 // String implements the Stringer interface for type BWhite.
 func (c BWhite) String() string {
 	return colConcat(BgWhite, string(c), BgDefault)
+}
+
+func BWhiteByte(b []byte) []byte {
+	return colConcatByte(BgWhite, b, BgDefault)
 }
 
 // BWhitef returns a BWhite formatted string.
@@ -343,6 +415,10 @@ func (c BBlack) String() string {
 	return colConcat(BgBlack, string(c), BgDefault)
 }
 
+func BBlackByte(b []byte) []byte {
+	return colConcatByte(BgBlack, b, BgDefault)
+}
+
 // BBlackf returns a BBlack formatted string.
 func BBlackf(format string, a ...any) string {
 	return BBlack(fmt.Sprintf(format, a...)).String()
@@ -351,6 +427,10 @@ func BBlackf(format string, a ...any) string {
 // String implements the Stringer interface for type BCyan.
 func (c BCyan) String() string {
 	return colConcat(BgCyan, string(c), BgDefault)
+}
+
+func BCyanByte(b []byte) []byte {
+	return colConcatByte(BgCyan, b, BgDefault)
 }
 
 // BCyanf returns a BCyan formatted string.
@@ -365,9 +445,17 @@ func (c Blinking) String() string {
 	return colConcat(Blink, string(c), NoMode)
 }
 
+func BlinkingByte(b []byte) []byte {
+	return colConcatByte(Blink, b, NoMode)
+}
+
 // String implements the Stringer interface for type Underline.
 func (c Underline) String() string {
 	return colConcat(Underln, string(c), NoMode)
+}
+
+func UnderlineByte(b []byte) []byte {
+	return colConcatByte(Underln, b, NoMode)
 }
 
 // String implements the Stringer interface for type Bold.
@@ -375,9 +463,17 @@ func (c Bold) String() string {
 	return colConcat(Bld, string(c), NoMode)
 }
 
+func BoldByte(b []byte) []byte {
+	return colConcatByte(Bld, b, NoMode)
+}
+
 // String implements the Stringer interface for type Italic.
 func (c Italic) String() string {
 	return colConcat(Ital, string(c), NoMode)
+}
+
+func ItalicByte(b []byte) []byte {
+	return colConcatByte(Ital, b, NoMode)
 }
 
 // NewColor gives a type Color back with specified fg/bg colors set that can
